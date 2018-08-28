d6cb0bbd50e7155319ae6a6b1bd1684def94339c
Nickolas Kenyeres <nickolas@knicklabs.com>
2018-08-28 19:27:32 -0400

    Setup typescript with linting

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+const apple = "apple";
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const PATHS = {
+  SRC: "./src",
+  DIST: path.resolve(__dirname)
+};
+
+module.exports = {
+  entry: {
+    "dist/index": `${PATHS.SRC}/index`,
+    "dist/bin/lorem-ipsum": `${PATHS.SRC}/bin/lorem-ipsum`
+  },
+
+  mode: "development",
+
+  output: {
+    path: PATHS.DIST,
+    filename: "[name].js",
+    library: "lorem-ipsum",
+    libraryTarget: "umd"
+  },
+
+  resolve: {
+    extensions: ["!.spec.ts", ".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)|(js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: conf => {
+        if (conf.filename === "dist/bin/lorem-ipsum.js") {
+          return "#!/usr/bin/env node";
+        }
+        return "";
+      },
+      raw: true
+    })
+  ]
+};
