c354aba057d3eb16a31530bf36dd123fdc076cc2
Nickolas Kenyeres <nickolas@knicklabs.com>
2018-09-01 09:08:09 -0400

    Wrap up porting to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,60 @@
-import { FORMAT_HTML, FORMAT_PLAIN, FORMATS } from "./constants/formats";
-import { LINE_ENDINGS } from "./constants/lineEndings";
-import Generator, { IGeneratorOptions, IPrng } from "./lib/generator";
-import { isNode, isReactNative, isWindows, makeArrayOfStrings } from "./util";
+import { WORDS } from "./constants/words";
+import { IPrng } from "./lib/generator";
+import LoremIpsum from "./lib/LoremIpsum";
 
-export class LoremIpsum {
-  public generator: Generator;
-  public format: string;
-  public suffix?: string;
-
-  constructor(
-    options: IGeneratorOptions,
-    format: string = FORMAT_PLAIN,
-    suffix: string,
-  ) {
-    if (FORMATS.indexOf(format.toLowerCase()) === -1) {
-      throw new Error(
-        `${format} is an invalid format. Please us ${FORMATS.join(" or ")}.`,
-      );
-    }
-
-    this.format = format.toLowerCase();
-    this.suffix = suffix;
-    this.generator = new Generator(options);
-  }
-
-  public getLineEnding() {
-    if (this.suffix) {
-      return this.suffix;
-    }
-
-    if (!isReactNative() && isNode() && isWindows()) {
-      return LINE_ENDINGS.WIN32;
-    }
-
-    return LINE_ENDINGS.POSIX;
-  }
-
-  public formatString(str: string): string {
-    if (this.format === FORMAT_HTML) {
-      return `<p>${str}</p>`;
-    }
-    return str;
-  }
-
-  public generateWords(num: number): string {
-    return this.formatString(this.generator.generateRandomWords(num));
-  }
-
-  public generateSentences(num: number): string {
-    return this.formatString(this.generator.generateRandomParagraph(num));
-  }
+export interface ILoremIpsumParams {
+  count?: number;
+  format?: string;
+  paragraphLowerBound?: number;
+  paragraphUpperBound?: number;
+  random?: IPrng;
+  sentenceLowerBound?: number;
+  sentenceUpperBound?: number;
+  units?: string;
+  words?: string[];
+  suffix?: string;
 }
 
-export default LoremIpsum;
+const loremIpsum = ({
+  count = 1,
+  format = "plain",
+  paragraphLowerBound = 3,
+  paragraphUpperBound = 7,
+  random,
+  sentenceLowerBound = 5,
+  sentenceUpperBound = 15,
+  units = "sentences",
+  words = WORDS,
+  suffix = "",
+}: ILoremIpsumParams): string => {
+  const options = {
+    random,
+    sentencesPerParagraph: {
+      max: paragraphUpperBound,
+      min: paragraphLowerBound,
+    },
+    words,
+    wordsPerSentence: {
+      max: sentenceUpperBound,
+      min: sentenceLowerBound,
+    },
+  };
+
+  const lorem: LoremIpsum = new LoremIpsum(options, format, suffix);
+
+  switch (units) {
+    case "paragraphs":
+    case "paragraph":
+      return lorem.generateParagraphs(count);
+    case "sentences":
+    case "sentence":
+      return lorem.generateSentences(count);
+    case "words":
+    case "word":
+      return lorem.generateWords(count);
+    default:
+      return "";
+  }
+};
+
+export default loremIpsum;
