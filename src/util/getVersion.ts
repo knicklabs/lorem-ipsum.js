ae4411c3cdaa5d814bdb6f3bb6c835c35f490169
Nickolas Kenyeres <nickolas@knicklabs.com>
2019-02-18 20:04:18 -0500

    Fix getting version number

--- src/util/getVersion.ts
+++ src/util/getVersion.ts
@@ -4,7 +4,7 @@ import fs from "fs";
  * @returns  The package version.
  */
 const getVersion = (): string => {
-  const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
+  const pkg = JSON.parse(fs.readFileSync(`${__dirname}/../../package.json`, "utf8"));
   return pkg.version;
 };
 
